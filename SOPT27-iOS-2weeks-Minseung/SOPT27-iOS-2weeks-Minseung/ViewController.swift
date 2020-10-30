8d1c2e91bee8c967bf53a8d8970a63f8dd3c0836
MinseungSeon <seonminseung@naver.com>
2020-10-30 18:02:54 +0900

    :pencil: Added week 2 assignment xcode project!

---
+++ SOPT27-iOS-2weeks-Minseung/SOPT27-iOS-2weeks-Minseung/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SOPT27-iOS-2weeks-Minseung
+//
+//  Created by 선민승 on 2020/10/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SOPT27-iOS-2weeks-Minseung/SOPT27-iOS-2weeks-MinseungTests/SOPT27_iOS_2weeks_MinseungTests.swift
@@ -0,0 +1,33 @@
+//
+//  SOPT27_iOS_2weeks_MinseungTests.swift
+//  SOPT27-iOS-2weeks-MinseungTests
+//
+//  Created by 선민승 on 2020/10/17.
+//
+
+import XCTest
+@testable import SOPT27_iOS_2weeks_Minseung
+
+class SOPT27_iOS_2weeks_MinseungTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
